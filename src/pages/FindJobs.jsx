6451ac90eb226e5094fcfa2725ca7d537ddbbc68
Amumug <fabriali4@gmail.com>
2024-09-08 13:17:36 +0100

    react-jobs

--- src/pages/FindJobs.jsx
+++ src/pages/FindJobs.jsx
@@ -1,27 +1,13 @@
+import { Button, Card } from "@mui/material";
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import JobCards from "../components/JobCards";
 
 const FindJobs = () => {
-  const [jobs, setJobs] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchJobs = async () => {
-      try {
-        const res = await fetch("http://localhost:8000/jobs");
-        const data = await res.json();
-        setjobs(data);
-      } catch (error) {
-        console("Error fetching data", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchJobs();
-  }, []);
-
-  return <div>{jobs.jobName}</div>;
+  return (
+    <>
+      <JobCards />
+    </>
+  );
 };
 
 export default FindJobs;
